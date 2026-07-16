180fd6fc8779412f7bc045611a74d7afbcfffc6e
Bharvadiya Aaryan V <aryanbharvadiya93@gmail.com>
2026-07-16 08:41:44 +0530

    stopwatch chnages

--- demo_application/lib/stopwatch/stopwatch.dart
+++ demo_application/lib/stopwatch/stopwatch.dart
@@ -5,10 +5,10 @@ class StopwatchExample extends StatefulWidget {
   const StopwatchExample({super.key});
 
   @override
-  State<StopwatchExample> createState() => _StopwatchExampleState();
+  State<StopwatchExample> createState() => _StopWatchExampleState();
 }
 
-class _StopwatchExampleState extends State<StopwatchExample> {
+class _StopWatchExampleState extends State<StopwatchExample> {
   int seconds = 0;
   late Timer timer;
   bool isRunning = false;
@@ -16,38 +16,53 @@ class _StopwatchExampleState extends State<StopwatchExample> {
   @override
   void initState() {
     super.initState();
-    timer = Timer.periodic(Duration(seconds: 1), _onTick);
   }
 
   @override
   void dispose() {
-    timer.cancel();
     super.dispose();
   }
 
   void _onTick(Timer timer) {
     setState(() {
-      seconds++;
+      if (isRunning) {
+        seconds++;
+      }
     });
   }
 
   void _startTimer() {
+    timer = Timer.periodic(const Duration(seconds: 1), _onTick);
     setState(() {
+      seconds = 0;
       isRunning = true;
     });
   }
 
   void _stopTimer() {
+    timer.cancel();
     setState(() {
       isRunning = false;
     });
   }
 
-  String _secondToText() => seconds == 1 ? '1 seconds' : '$seconds seconds';
+  void _pauseTimer() {
+    setState(() {
+      isRunning = false;
+    });
+  }
 
+  void _resumeTimer() {
+    setState(() {
+      isRunning = true;
+    });
+  }
+
+  String _secondToText() => seconds == 1 ? '1 second' : '$seconds seconds';
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('stopWatch Example')),
+      appBar: AppBar(title:const Text('Stopwatch Example')),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
@@ -59,19 +74,48 @@ class _StopwatchExampleState extends State<StopwatchExample> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               ElevatedButton(
-                onPressed: _startTimer,
+                onPressed: isRunning ? null : _startTimer,
                 style: ButtonStyle(
-                  backgroundColor: WidgetStateProperty.all<Color>(Colors.green),
-                  foregroundColor: WidgetStateProperty.all<Color>(Colors.white),
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                    Colors.green,
+                  ),
+                  foregroundColor: MaterialStateProperty.all<Color>(
+                    Colors.white,
+                  ),
                 ),
                 child: const Text('Start'),
               ),
-              const SizedBox(width: 20),
               ElevatedButton(
-                onPressed: _stopTimer,
+                onPressed: isRunning ? _pauseTimer : null,
                 style: ButtonStyle(
-                  backgroundColor: WidgetStateProperty.all<Color>(Colors.red),
-                  foregroundColor: WidgetStateProperty.all<Color>(Colors.white),
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                    Colors.orange,
+                  ),
+                  foregroundColor: MaterialStateProperty.all<Color>(
+                    Colors.white,
+                  ),
+                ),
+                child: const Text('Pause'),
+              ),
+              ElevatedButton(
+                onPressed: isRunning ? null : _resumeTimer,
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                    Colors.blue,
+                  ),
+                  foregroundColor: MaterialStateProperty.all<Color>(
+                    Colors.white,
+                  ),
+                ),
+                child: const Text('Resume'),
+              ),
+              ElevatedButton(
+                onPressed: isRunning ? _stopTimer : null,
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+                  foregroundColor: MaterialStateProperty.all<Color>(
+                    Colors.white,
+                  ),
                 ),
                 child: const Text('Stop'),
               ),
